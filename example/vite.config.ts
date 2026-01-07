1f4dcdd96d6c2cacd87ebc16da00401aa5dab388
Mayank Bisht <octpahadi@gmail.com>
2026-01-08 02:26:04 +0530

    feat: example sandbox v1

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/pixel-colors/",
-  server: {
-    port: 3000,
+  resolve: {
+    alias: {
+      "pixel-colors": path.resolve(__dirname, "../src/index.ts"),
+    },
+    dedupe: ["react", "react-dom"],
   },
 });
