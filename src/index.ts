2e5cd0a46cf75edfb1ca877f98956e96260f98a6
kiritocode1 <kathawalearyan9@gmail.com>
2026-01-08 17:02:59 +0530

    chore: improve search speed insights

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 export * from "./extractColors";
 export * from "./usePixelColors";
 export * from "./utils";
+
+
+// next ver : https://tweakcn.com/editor/theme?tab=ai
+//https://ui.shadcn.com/docs/registry/getting-started 
